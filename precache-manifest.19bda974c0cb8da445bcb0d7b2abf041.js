8515447a2e0d3cde42d04e55b3bae9c0a95e9250
Tim Malaney <tmalaney@usc.edu>
2019-01-16 08:26:46 -0800

    Updates

---
+++ precache-manifest.19bda974c0cb8da445bcb0d7b2abf041.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/roskimag-website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bde3068f171624465ebd",
+    "url": "/roskimag-website/static/js/runtime~main.bde3068f.js"
+  },
+  {
+    "revision": "e5cf5dbc0fd4c5de9c46",
+    "url": "/roskimag-website/static/js/main.e5cf5dbc.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/roskimag-website/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "e5cf5dbc0fd4c5de9c46",
+    "url": "/roskimag-website/static/css/main.e592d708.chunk.css"
+  },
+  {
+    "revision": "4615c049872336015b26935db9e9b2e5",
+    "url": "/roskimag-website/index.html"
+  }
+];
